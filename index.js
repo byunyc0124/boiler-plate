3ac2ab1e64a7a9a081d18dde21baf0d29c49c179
byunyc0124 <baeyonyk124@gmail.com>
2022-08-27 22:09:20 +0900

    feat: 회원가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 // mongoDB 연결
 const mongoose = require('mongoose')
@@ -11,6 +18,19 @@ mongoose.connect('mongodb+srv://byc:abcd1234@boilerplate.y5ld8ld.mongodb.net/?re
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보들은 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
