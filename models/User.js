ed7896e154ce53eeae9482891e3a365d3adeb617
byunyc0124 <baeyonyk124@gmail.com>
2022-09-10 22:04:52 +0900

    feat: 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name:{
@@ -28,6 +31,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp:{
         type: Number
+    }
+})
+
+// save 하기 전에 실행할 함수
+userSchema.pre('save', function(next){
+    var user = this;
+
+    // 비밀번호를 변경하는 경우에만 실행
+    if(user.isModified('password')){
+        // 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
