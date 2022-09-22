dc0bd13fb963d5a070123f9a82250dd5a5ef50bc
byunyc0124 <baeyonyk124@gmail.com>
2022-09-22 23:33:33 +0900

    feat: auth 인증체크

--- client/src/App.js
+++ client/src/App.js
@@ -9,15 +9,19 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
 
 function App() {
+  const AuthenticLandingPage = Auth({LandingPage}, null)
+  const AuthenticLoginPage = Auth({LoginPage}, false)
+  const AuthenticRegisterPage = Auth({RegisterPage}, false)
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/" element={<AuthenticLandingPage />} />
+          <Route exact path="/login" element={<AuthenticLoginPage />} />
+          <Route exact path="/register" element={<AuthenticRegisterPage />} />
         </Routes>
       </div>
     </Router>
